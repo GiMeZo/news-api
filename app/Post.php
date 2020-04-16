d0bd06a2e04098074b93a721e8af586d58d4b3a4
GiMeZo <salah@marocplex.com>
2020-04-16 19:52:04 +0000

    Add Migration

---
+++ app/Post.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $fillable = [
+        'title', 'content', 'featured_image', 'votes_up',
+        'votes_down', 'user_id', 'category_id'
+    ];
+}
